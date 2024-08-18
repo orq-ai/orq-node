bc9526a8a6fbf0127717cab1bd17a04b25e3d8cd
thedevtoni <antony280991@gmail.com>
2024-08-18 21:34:58 +0200

    feat(core): remove axios from the dependencies

--- orq-ai-sdk/src/lib/http/index.ts
+++ orq-ai-sdk/src/lib/http/index.ts
@@ -1,24 +1,23 @@
 import { Store } from "../utils";
 
-// Create a function that allow to create an http request with axios
 export async function createHttpRequest<T extends { [key: string]: any }>({
-	method = "POST",
-	url,
-	data,
+  method = "POST",
+  url,
+  data,
 }: {
-	method: string;
-	url: string;
-	data?: T;
+  method: string;
+  url: string;
+  data?: T;
 }) {
-	return fetch(`https://api.orq.ai/${url}`, {
-		method,
-		headers: {
-			"Content-Type": "application/json",
-			Accept: "application/json",
-			Authorization: `Bearer ${Store.apiKey}`,
-			...(Store.contactId && { "X-Orq-Contact-Id": Store.contactId }),
-			...(data?.["stream"] && { Accept: "text/event-stream" }),
-		},
-		body: JSON.stringify(data),
-	});
+  return fetch(`https://api.orq.ai/${url}`, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${Store.apiKey}`,
+      ...(Store.contactId && { "X-Orq-Contact-Id": Store.contactId }),
+      ...(data?.["stream"] && { Accept: "text/event-stream" }),
+    },
+    body: JSON.stringify(data),
+  });
 }
